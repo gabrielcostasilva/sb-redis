ae2c587833c09df183d13fa0b38b04ac28d92f18
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2023-05-11 10:14:59 -0300

    Added redis-session project

--- src/main/java/com/example/redissession/RedisSessionApplication.java
+++ src/main/java/com/example/redissession/RedisSessionApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.redissession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class RedisSessionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(RedisSessionApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/redissession/RedisSessionApplicationTests.java
+++ src/test/java/com/example/redissession/RedisSessionApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.redissession;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class RedisSessionApplicationTests {
 
 	@Test
 	void contextLoads() {
